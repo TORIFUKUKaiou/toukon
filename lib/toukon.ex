ce442fa6a868fe7c4bd813319baf07e2a949e4de
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2023-01-29 21:13:05 +0900

    delete Toukon.hello/1

--- lib/toukon.ex
+++ lib/toukon.ex
@@ -2,17 +2,4 @@ defmodule Toukon do
   @moduledoc """
   Documentation for `Toukon`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Toukon.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/toukon_test.exs
+++ test/toukon_test.exs
@@ -1,8 +1,4 @@
 defmodule ToukonTest do
   use ExUnit.Case
   doctest Toukon
-
-  test "greets the world" do
-    assert Toukon.hello() == :world
-  end
 end
